1989be5b8e32a7b9b0c060eaf8b89e3678e2de3c
ronghongbo <hongbo.rong@intel.com>
2023-03-07 22:54:04 -0800

    Modify parse options to accept QRD

--- t2s/tests/performance/parse-options.sh
+++ t2s/tests/performance/parse-options.sh
@@ -18,7 +18,7 @@ else
     location="$1"
 fi
 
-if [ "$2" != "gemm" -a "$2" != "conv"  -a  "$2" != "capsule" -a "$2" != "pairhmm" ]; then
+if [ "$2" != "gemm" -a "$2" != "conv"  -a  "$2" != "capsule" -a "$2" != "pairhmm" -a "$2" != "qrd"]; then
     show_usage
     return
 else
